2755cf97cc14c83482387e5d7acba13acdb1af16
Dmitry <manxxeta@gmail.com>
2018-09-19 15:35:04 -0700

    starting comments

--- server.js
+++ server.js
@@ -128,11 +128,11 @@ function Business(business)
   this.url = business.url;
 }
 
-function Movie(movie) {
+function Movie(movie)
+{
   this.title = movie.title;
   this.overview = movie.overview;
   this.average_votes = movie.vote_average;
-  // this.total_votes = this.votes_total;
   this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   this.popularity = movie.popularity;
   this.released_on = movie.release_date;
